23171ad94ca20a99804104c9d1262af75669f344
Osama Alfarsi <osamaalfarsi17@gmail.com>
2026-05-11 22:37:14 +0400

    force backend update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,10 +10,10 @@ app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
 
 app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/parking', require('./routes/parkingRoutes'))
 
 app.listen(5000, () => {
-  console.log('Server running on 5000')
+  console.log('Server running')
 })
