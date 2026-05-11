198a1bc617bf0db66881a5274a50ed5e06a82dfb
Osama Alfarsi <osamaalfarsi17@gmail.com>
2026-05-11 22:20:12 +0400

    force backend update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
 
 app.use(cors())
 app.use(express.json())
