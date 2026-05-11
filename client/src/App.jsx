9ef8c6c2c08f389a063662a99bd414bc37852961
Osama Alfarsi <osamaalfarsi17@gmail.com>
2026-05-11 21:23:49 +0400

    fixed vercel routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import NavigationBar from './components/Navbar'
+
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
@@ -8,17 +10,17 @@ import EventPage from './pages/EventPage'
 function App() {
   return (
     <BrowserRouter>
+
+      {/* ✅ Navbar هنا يظهر في كل الصفحات */}
+      <NavigationBar />
+
       <Routes>
-
         <Route path='/' element={<HomePage />} />
-
         <Route path='/login' element={<LoginPage />} />
-
         <Route path='/register' element={<RegisterPage />} />
-
         <Route path='/events' element={<EventPage />} />
-
       </Routes>
+
     </BrowserRouter>
   )
 }
