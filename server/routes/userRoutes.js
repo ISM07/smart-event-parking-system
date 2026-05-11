6e85b437f40983cbfd98d046c3a4b8948ac5841e
Osama Alfarsi <osamaalfarsi17@gmail.com>
2026-05-11 22:16:51 +0400

    force backend update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,4 @@
-const router = require('express').Router()
-const User = require('../models/User')
-
 router.post('/register', async (req, res) => {
   const user = await User.create(req.body)
   res.json(user)
 })
-
-module.exports = router
